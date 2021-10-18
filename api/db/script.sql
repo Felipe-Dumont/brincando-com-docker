d9e4673b307cd682c3f427f44fa3428133b6c7bc
Felipe Dumont De Sa Costa <50582912+Felipe-Dumont@users.noreply.github.com>
2021-10-18 10:55:10 -0300

    Update script.sql

--- api/db/script.sql
+++ api/db/script.sql
@@ -9,6 +9,6 @@ CREATE TABLE IF NOT EXISTS products (
     PRIMARY KEY (id)
 );
 
-INSERT INTO products VALUE(0, 'Caneca de seramica, perfeita para café', 45);
+INSERT INTO products VALUE(0, 'Caneca de ceramica, perfeita para café', 45);
 INSERT INTO products VALUE(0, 'Copo de acrilico, perfeito para agua', 29);
 INSERT INTO products VALUE(0, 'Garrafa de platico com tampa, perfeito para agua', 29);
