d224582a298a3fef01abd2a2eaa277a407c85eae
felipe dumont <felipe00.dumont@gmail.com>
2021-10-18 10:33:44 -0300

    adicionando arquivo docker composer

--- api/src/index.js
+++ api/src/index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const app = express();
 
 const connection = mysql.createConnection({
-    host: 'frosty_edison',
+    host: 'mysql-container',
     user: 'root',
     password: 'felipedocker',
     database: 'felipedocker'
